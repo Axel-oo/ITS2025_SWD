dd0483b9a9918053ab148b033da88f0979e787b3
Axel-oo <andrea.alume@edu-its.it>
2024-12-17 12:24:33 +0100

    aggiunta la prima lezione

---
+++ SQL/query1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE corsi (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    ore INT NULLcorsi
+    -- PRIMARY KEY (id) <-- questo è un altro modo per scrivere la le istruzioni di chiave primaria --
+);
